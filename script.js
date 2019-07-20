e0806f71ff22d08c9ca82248c495195e4e47bb9b
SYED ALEEM HUSSAIN <49521238+sahzaleem123@users.noreply.github.com>
2019-07-20 14:42:29 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
 
 let result = document.querySelector("h1");
-
+let intelligence = [
+    " Very Intelligent",
+    "Quite Impressive",
+    "Very Creative",
+    "Very Sharp",
+    "Very Keen",
+    "Very Smart",
+    "Very Penetreting",
+    "Very Dumb",
+    "Very Idiot"
+];
 function quizApp(name) {
 
     let randomNumber = parseInt(Math.random() * 11);
+    result.innerHTML = "Dear " + name + " You are" + intelligence[randomNumber];
 
-    if (randomNumber === 1) {
-        result.innerHTML = name + "You are very Intelligent"
-    } else if (randomNumber === 2) {
-        result.innerHTML = name + "You are quite Impressive"
-    } else if (randomNumber === 3) {
-        result.innerHTML = name + "You are very Creative"
-    } else if (randomNumber === 4) {
-        result.innerHTML = name + "You are very Sharp"
-    } else if (randomNumber === 5) {
-        result.innerHTML = name + "You are very Keen"
-    } else if (randomNumber === 6) {
-        result.innerHTML = name + "You are very Smart"
-    } else if (randomNumber === 7) {
-        result.innerHTML = name + "You are very Retard"
-    } else if (randomNumber === 8) {
-        result.innerHTML = name + "You are very Penetreting"
-    } else if (randomNumber === 9) {
-        result.innerHTML = name + "You are very Dumb"
-    } else if (randomNumber === 10) {
-        result.innerHTML = name + "You are very Idiot"
-    }
+
 }
 document.querySelector("button").addEventListener('click', function () {
     quizApp(document.querySelector("input").value.toUpperCase());
